c8425723b17fbd40947f9af0c78c93d7db32fb92
Ismael Mira <ismaelmira333@gmail.com>
2026-02-20 00:16:33 -0500

    mi nuevo cambio

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() { 
+  const fecha = new Date().toLocaleString(); 
+return ( <main> <h1>"Hola, soy Ismael - Aprendiz ADSO". 
+  React es una librería de JavaScript que estoy aprendiendo <br />
+  para crear interfaces web de forma simple y organizada. <br />
+  Me permite entender mejor cómo funcionan las aplicaciones <br />
+  y seguir mejorando en desarrollo web. <br />
+  {fecha}</h1> </main> )}
